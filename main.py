f97dbf235e699fb05272cc3a8f931e355f46f219
Jose Luis Colmenares <jlrandmore.me@gmail.com>
2024-01-26 23:31:31 +0100

    Return movies - segundo endpoint de la API

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ app = FastAPI()
 app.title = "Mi aplicación con FastAPI"
 app.version = "0.0.1"
 
+
+movies = [
+    {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
+]
+
 @app.get('/', tags=['home']) #ruta de inicio
 def message():
     return HTMLResponse('<h1>Hellow world</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
